1e229a28397a371107936f09a07fa93a3ad86faa
Jack H <8584913+jckhxn@users.noreply.github.com>
2023-08-14 06:34:37 -0400

    social media links

--- src/components/Footer/static-data.tsx
+++ src/components/Footer/static-data.tsx
@@ -6,8 +6,21 @@ export const footer: {
   companyLogo: StaticImageData;
   incorporatedDate: number;
   currentYear: number;
+  linkedin: string;
+  facebook: string;
+  instagram: string;
+  twitter: string;
+  tiktok: string;
+  youtube: string;
 } = {
   companyLogo: logo,
   incorporatedDate: 2023,
   currentYear: new Date().getFullYear(),
+  linkedin:
+    "https://www.linkedin.com/company/growing-your-business-with-people",
+  facebook: "https://www.facebook.com/GYBWPpodcast",
+  instagram: "https://www.instagram.com/growingyourbusinesswithpeople/",
+  twitter: "https://twitter.com/GYBWPpodcast",
+  tiktok: "https://www.tiktok.com/@gybwp",
+  youtube: "https://www.youtube.com/@jkladvisors",
 };
