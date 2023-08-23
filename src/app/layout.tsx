8cc0b34fede5ba99af52a68133b97f0a90163ca5
Jack H <8584913+jckhxn@users.noreply.github.com>
2023-08-23 19:49:44 -0400

    moved index to app dir

---
+++ src/app/layout.tsx
@@ -0,0 +1,57 @@
+"use client";
+import React from "react";
+import { usePathname } from "next/navigation";
+
+// components
+import DefaultLayout from "components/Layout";
+import Footer from "components/Footer";
+import Header from "components/Header";
+
+// styling
+import "tailwindcss/tailwind.css";
+import "/dist/output.css";
+import { Open_Sans } from "next/font/google";
+const openSans = Open_Sans({ subsets: ["latin"], weight: "variable" });
+
+// types
+import { NextComponentType } from "next";
+import type { AppProps } from "next/app";
+
+//
+//
+//
+//
+// DO NOT TOUCH THIS FILE UNLESS YOU'RE A DEV
+
+type Component = NextComponentType & {
+  Layout?: React.FC & { background?: string };
+};
+
+export type ApplicationProps = AppProps & {
+  Component: Component;
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const Layout = DefaultLayout;
+  const pathname = usePathname();
+
+  return (
+    <>
+      <html lang="en">
+        <body>
+          <Layout>
+            <main className={openSans.className}>
+              <Header />
+              {children}
+              <Footer />
+            </main>
+          </Layout>
+        </body>
+      </html>
+    </>
+  );
+}
---
+++ src/app/static-data.tsx
@@ -0,0 +1,71 @@
+import routes from "routes";
+import * as podcast from "../app/episode-data";
+
+// types
+import { seasonType, episodeType } from "../app/episode-data";
+
+//
+//
+//
+//
+// ONLY TOUCH THINGS BELOW THIS LINE
+
+export const HERO = {
+  header: "Welcome to Growing your business with People!",
+  body: "Our podcast, “Growing your business with People!” is dedicated to CEOs and Business Leaders who understand that the key to growth is found in their biggest and most important investment - people!",
+  buttonText: "Watch Now",
+  buttonUrl: routes.external.subscribe,
+  secondButtonText: "Listen Now",
+  secondButtonUrl: routes.external.listen,
+};
+
+export const CTA = {
+  header: "Growing your business with People!",
+  body: "Growing your business with your biggest and most important investment - People!",
+  buttonText: "Subscribe to our Newsletter!",
+  buttonUrl: routes.external.newsletter,
+};
+
+export const PODCAST: seasonType[] = [
+  podcast.SEASON_ONE,
+  podcast.SEASON_TWO,
+  podcast.SEASON_THREE,
+];
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// DO NOT TOUCH ANYTHING BELOW THIS LINE
+
+export const getEpisodesBySeason = (seasonToFind: number) => {
+  const foundSeason = PODCAST.filter(
+    (season) => season.seasonNumber === seasonToFind
+  )[0];
+
+  return [...foundSeason.episodes].reverse();
+};
+
+export const getEpisodesBySponsor = (uuid: string) => {
+  const sponsoredEpisodes: episodeType[] = [];
+
+  PODCAST.forEach((season) => {
+    season.episodes.forEach((episode) => {
+      episode.sponsors.forEach((id) => {
+        if (id === uuid) {
+          sponsoredEpisodes.push(episode);
+        }
+      });
+    });
+  });
+
+  return sponsoredEpisodes;
+};
