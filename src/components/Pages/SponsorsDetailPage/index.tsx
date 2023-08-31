95c5a940ce7de54a0c72cfa399e74e0eaff15008
Jack H <8584913+jckhxn@users.noreply.github.com>
2023-08-31 00:46:25 -0400

    messy

--- src/components/Pages/SponsorsDetailPage/index.tsx
+++ src/components/Pages/SponsorsDetailPage/index.tsx
@@ -1,5 +1,6 @@
+"use client";
 import React, { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 // components
 import Button from "components/Button";
