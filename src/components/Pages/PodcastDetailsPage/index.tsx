a0adcfa188eac26ea0a003c3f822c4cfda14123f
Jack H <8584913+jckhxn@users.noreply.github.com>
2023-08-31 01:31:32 -0400

    switched to next/router will that work?

--- src/components/Pages/PodcastDetailsPage/index.tsx
+++ src/components/Pages/PodcastDetailsPage/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/router";
 import routes from "routes";
 
 // components
