7df23929162573e3fed55d85f6a041e09fcb5783
Thomas Hixon <8584913+jckhxn@users.noreply.github.com>
2023-09-28 22:25:11 -0400

    layout now uses metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-"use client";
 import React from "react";
 import { usePathname } from "next/navigation";
 
@@ -43,7 +42,6 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   const Layout = DefaultLayout;
-  const pathname = usePathname();
 
   return (
     <>
