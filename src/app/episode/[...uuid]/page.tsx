d34cb8711fb5e9b08d4a85da429d961c63c14000
Thomas Hixon <8584913+jckhxn@users.noreply.github.com>
2023-10-04 15:12:05 -0400

    undid video embed stuff

--- src/app/episode/[...uuid]/page.tsx
+++ src/app/episode/[...uuid]/page.tsx
@@ -21,8 +21,6 @@ export const generateMetadata = async (props: Props): Promise<Metadata> => {
         title: episodeDetails.episodeName,
         description: episodeDetails.blurb,
         image: episodeDetails.image,
-        type: "video.other",
-        url: "https://www.youtube.com/watch?v=Mp-dMDn9Afw",
         images: [
           {
             url: episodeDetails.image,
