86b36f37f60ef4834aba0b8e2df32c1837a7722f
Thomas Hixon <8584913+jckhxn@users.noreply.github.com>
2024-04-10 11:46:01 -0400

    removed dumb data

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -106,20 +106,6 @@ const Slider: React.FC<{
 
     return standardEpisode;
   };
-  const works: Artwork[] = [
-    {
-      artist: "Ornella Binni",
-      art: "https://images.unsplash.com/photo-1465869185982-5a1a7522cbcb?auto=format&fit=crop&w=300&q=80",
-    },
-    {
-      artist: "Tom Byrom",
-      art: "https://images.unsplash.com/photo-1548516173-3cabfa4607e9?auto=format&fit=crop&w=300&q=80",
-    },
-    {
-      artist: "Vladimir Malyavko",
-      art: "https://images.unsplash.com/photo-1494337480532-3725c85fd2ab?auto=format&fit=crop&w=300&q=80",
-    },
-  ];
 
   return (
     <>
