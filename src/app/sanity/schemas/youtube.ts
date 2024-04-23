a84fab1abeef2639541bc7df50371d3f45309c89
Thomas Hixon <8584913+jckhxn@users.noreply.github.com>
2024-04-23 12:52:11 -0400

    youtube plugin release

--- src/app/sanity/schemas/youtube.ts
+++
@@ -1,9 +0,0 @@
-import { defineType } from "sanity";
-export const youtube = defineType({
-  name: "youtubeEmbed",
-  type: "document",
-  fields: [
-    { name: "title", type: "string" },
-    { name: "video", type: "youtubeVideo" },
-  ],
-});
