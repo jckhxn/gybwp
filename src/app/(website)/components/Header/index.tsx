9f8888792ea1ba6358b345489026fd4a60ab1bea
Thomas Hixon <8584913+jckhxn@users.noreply.github.com>
2024-05-22 19:57:47 -0400

    chore: Update paths in tsconfig.json for website components

---
+++ src/app/(website)/components/Header/index.tsx
@@ -0,0 +1,134 @@
+"use client";
+import React, { useState } from "react";
+import routes from "@/src/app/(website)/routes";
+
+// components
+import Link from "next/link";
+import Image from "next/image";
+import logo from "images/logo.webp";
+
+// static data
+import { PAGES, COPY } from "./static-data";
+
+const Navigation = () => {
+  const [mobileNavOpen, setMobileNavOpen] = useState<boolean>(false);
+
+  const handleMobileHamburgerClick = () => {
+    setMobileNavOpen(!mobileNavOpen);
+  };
+
+  return (
+    <header aria-label="Site Header" className="bg-white pt-2">
+      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+        <div className="flex h-16 items-center justify-between">
+          <div className="flex-1 md:flex md:items-center md:gap-12">
+            <Link className="flex" href={routes.internal.home}>
+              <span className="sr-only">Home</span>
+              <Image
+                priority
+                loading="eager"
+                className="h-16 w-auto"
+                alt="Growing Your Business with People Logo"
+                src={logo}
+              />
+              <p className="self-center hidden ml-0 text-sm lg:text-xl md:block lg:ml-3">
+                Growing Your Business With People
+              </p>
+            </Link>
+          </div>
+
+          <div className="md:flex md:items-center md:gap-12">
+            {/* NAVIGATION LINKS */}
+            <nav aria-label="Site Nav" className="hidden md:block">
+              <ul className="flex items-center gap-6 text-sm">
+                {PAGES.map(({ name, url }) => (
+                  <li key={name}>
+                    <Link
+                      className="text-secondary transition hover:text-gray-500/75"
+                      href={url}
+                    >
+                      {name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+
+            <div className="flex items-center gap-4">
+              {/* FOLLOW US BUTTON */}
+              <div className="sm:flex sm:gap-4">
+                <Link
+                  className="rounded-md bg-main px-5 py-2.5 text-sm font-medium text-white shadow"
+                  href={routes.external.follow}
+                  target="_blank"
+                >
+                  {COPY.buttonText}
+                </Link>
+              </div>
+
+              {/* MOBILE NAVIGATION BUTTON */}
+              <div className="block md:hidden relative">
+                <button
+                  onClick={handleMobileHamburgerClick}
+                  className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75"
+                >
+                  <span className="sr-only">Open Navigation Menu</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                </button>
+
+                {/* MOBILE NAVIGATION DROPDOWN MENU */}
+                {mobileNavOpen && (
+                  <div
+                    className="absolute end-0 z-10 mt-2 w-56 rounded-md border border-gray-100 bg-white shadow-lg"
+                    role="menu"
+                  >
+                    <div className="p-2">
+                      <div className="flow-root">
+                        <nav
+                          aria-label="Millie Navigation"
+                          className="-my-2 flex flex-col divide-y divide-gray-100"
+                        >
+                          <ul className="space-y-1 py-2">
+                            {PAGES.map(({ name, url }) => (
+                              <li
+                                key={name}
+                                onClick={handleMobileHamburgerClick}
+                              >
+                                <Link
+                                  key={name}
+                                  href={url}
+                                  className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-main"
+                                >
+                                  {name}
+                                </Link>
+                              </li>
+                            ))}
+                          </ul>
+                        </nav>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navigation;
---
+++ src/app/(website)/lib/queries.ts
@@ -0,0 +1,113 @@
+// PUT ALL QUERIES HERE EVENTUALLY
+// ./sanity/lib/queries.ts
+
+import { groq } from "next-sanity";
+
+// Get Seasons
+
+export const SEASON_QUERY = groq`*[_type == "season"]`;
+
+// Guest Details query (by URL param -> slug)
+export const GUEST_QUERY = groq` *[_type == "guest" && slug.current == $slug] {
+  ...,
+
+"episodes": *[_type == "episode" && references('guests', ^._id)]
+{
+  ...,
+  "uuid":coalesce(uuid,youtube.uuid),
+  "image":coalesce(image,youtube.thumbnail),
+  "seasonNumber":coalesce(seasonNumber,youtube.seasonNumber),
+  "episodeNumber":coalesce(seasonNumber,youtube.episodeNumber)
+}
+}`;
+
+// Get all Episodes by UUID
+export const EPISODES = groq`*[_type == "episode"]| order(uuid asc){uuid}`;
+
+// Get all Episodes for sitemap
+export const ALL_EPISODES = groq`*[_type == "episode"]{
+  uuid,
+  _createdAt
+ }`;
+// Get episode details
+export const EPISODES_DETAILS_QUERY = groq`*[_type == "episode" && uuid == $uuid]{
+  episodeName,
+  blurb,
+  image,
+  url,
+}`;
+// Gets the latest season
+export const INITIAL_SEASON_EPISODES_QUERY = groq`{
+
+  "latestSeasonNumber":count(array::unique(*[_type == "episode"].seasonName))
+}
+`;
+// Get episodes homepage.
+export const SEASON_EPISODES_QUERY = groq`*[_type == "episode" && coalesce(seasonNumber,youtube.seasonNumber) == $seasonNumber]|order(uuid desc){...,
+  "uuid":coalesce(uuid,youtube.uuid),
+  "image":coalesce(image,youtube.thumbnail),
+  "seasonNumber":coalesce(seasonNumber,youtube.seasonNumber),
+  "episodeNumber":coalesce(seasonNumber,youtube.episodeNumber)
+}`;
+
+//  Total Seasons with Episodes Query
+export const TOTAL_SEASONS_QUERY = groq`{"seasonName":array::unique(*[_type == "episode"].seasonName),"seasonNumber":array::unique(*[_type == "episode" ].seasonNumber)}`;
+
+// Get details for current Podcast.
+export const PODCAST_DETAILS_QUERY = groq`*[_type == "episode" && coalesce(uuid,youtube.uuid) == $uuid] {
+    ...,
+    content {
+      files[] {
+        link,
+        name,
+        type,
+        "file": pdf.asset->url,
+        "image": image.asset->url
+      }
+    },
+    "blurb": coalesce(youtube.blurb, blurb),
+    "episodeName": coalesce(youtube.title, episodeName),
+    "episodeNumber": coalesce(youtube.episodeNumber, episodeNumber),
+    "image": coalesce(youtube.thumbnail, image),
+    "seasonNumber": coalesce(youtube.seasonNumber, seasonNumber),
+    "url": coalesce("https://www.youtube.com/" + youtube.id, url),
+    "uuid": coalesce(youtube.uuid, uuid),
+    guests[]->,
+    details {
+    ...,
+      featuredGuests[] {
+        ...,
+        "image": image.asset->url,
+       "episodes": *[_type=="episode" && details.featuredGuests[].name match ^.name && !(uuid == $uuid)] {
+      ...,
+      "episodeName": coalesce(youtube.title, episodeName),
+      "episodeNumber": coalesce(youtube.episodeNumber, episodeNumber),
+      "url": coalesce("https://www.youtube.com/" + youtube.id, url),
+      
+    }
+      }
+    },
+    "allParts":*[_type == "episode" && uuid != $uuid && uuid match $epID] | order(uuid asc) 
+,
+    "nextEpisode": *[_type == "episode" && _createdAt > ^._createdAt && uuid != ^._id] | order(_createdAt asc, uuid asc)[0].uuid,
+    "prevEpisode": *[_type == "episode" && _createdAt < ^._createdAt && uuid != ^._id] | order(_createdAt desc, uuid desc)[0].uuid,
+    "sponsors":*[_type=="sponsor" && uuid in ^.sponsors]
+}
+`;
+
+// Get specific Sponsor
+export const SPONSOR_DETAILS_QUERY = groq`{"sponsors":*[_type == "sponsor" && uuid == $id] ,"episodes":*[_type == "episode" &&  $id in sponsors]| order(uuid desc ){
+  "uuid":coalesce(uuid,youtube.uuid),
+    "image":coalesce(image,youtube.thumbnail),
+    "seasonNumber":coalesce(seasonNumber,youtube.seasonNumber),
+    "episodeNumber":coalesce(seasonNumber,youtube.episodeNumber)
+  }}`;
+
+// Get all Sponsors
+export const ALL_SPONSORS_QUERY = groq`*[_type == "sponsor"]`;
+
+// Featured News Articles
+export const FEATURED_ARTICLES_QUERY = groq`*[_type == "featuredArticle"]`;
+
+// Other Articles Query
+export const OTHER_ARTICLES_QUERY = groq`*[_type == "article"]`;
