da78dedc7f8ee89eaa5570843da2cd9ac74460b1
Thomas Hixon <8584913+jckhxn@users.noreply.github.com>
2024-05-22 15:23:44 -0400

    feat: Remove deprecated season schema
    The season schema has been removed as it is no longer needed in the project.

--- src/app/(website)/sanity/schemas/index.ts
+++ src/app/(website)/sanity/schemas/index.ts
@@ -3,7 +3,7 @@ import episode from "./episode";
 import featuredArticle from "./featuredArticles";
 import sponsor from "./sponsor";
 import guest from "./guest";
-import season from "./season";
+// import season from "./season";
 
 const schemas = [episode, guest, sponsor, article, featuredArticle];
 export default schemas;
