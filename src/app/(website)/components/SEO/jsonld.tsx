d02cd84e774d3114555ac7fe6406b1eec31bbaca
Thomas Hixon <8584913+jckhxn@users.noreply.github.com>
2024-06-13 15:11:38 -0400

    Added structured data component

--- src/app/(website)/components/SEO/jsonld.tsx
+++ src/app/(website)/components/SEO/jsonld.tsx
@@ -1,23 +1,13 @@
-import Script from "next/script";
 import React from "react";
 
-import type { BlogPosting, WithContext } from "schema-dts";
-
-const schema: WithContext<BlogPosting> = {
-  "@context": "https://schema.org",
-  "@type": "BlogPosting",
-  headline: "Welcome to my website",
-  description: "A blog post welcoming you to my website",
-  image: "https://example.com/photos/1x1/photo.jpg",
-};
-type Props = { data: any };
+type Props = { data: JSON };
 
 const JSONLD = (data: Props) => {
   return (
     <script
       type="application/ld+json"
       key="jsonld-data"
-      dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
     />
   );
 };
