3f0e771e5a81caa9c74d64e5d220516b1c3eed7e
Thomas Hixon <8584913+jckhxn@users.noreply.github.com>
2025-03-01 15:04:44 -0500

    removed .env

--- config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  youtubeChannelId: process.env.NEXT_PUBLIC_YOUTUBE_CHANNEL_ID,
-  youtubeApiKey: process.env.NEXT_PUBLIC_YOUTUBE_API_KEY,
-  sanityProjectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  sanityDataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  sanityReadToken: process.env.SANITY_READ_TOKEN,
-};
-
-export default config;
